bb85534769e7983bec283e9670a2518a4d8ab21e
Jan20199 <jan20199@email.vccs.edu>
2024-09-07 13:02:40 -0400

    commit

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+function List(props){
+
+    const category = props.category;
+    const itemList = props.items;
+
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); //REVERSE ALPH
+    // fruits.sort((a,b) => a.calories - b.calories);      //NUMERIC
+    // fruits.sort((a,b) => b.calories - a.calories);      //REVERSE NUME
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+    
+
+    const listItems = itemList.map(item =>   <li key={item.name}>
+                                            {item.name}: &nbsp;
+                                            <b>{item.calories}</b></li>);
+
+    return(<>
+                <h3 className = "list-category">{category}</h3>
+                <ol className = "list-items">{listItems}</ol>
+        </>);
+
+}
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({  id: PropTypes.number,
+                                                name: PropTypes.string,
+                                                calories: PropTypes.number})),
+}
+
+List.defaultProps = {
+    category: "Category",
+    items: [],
+}
+
+export default List
